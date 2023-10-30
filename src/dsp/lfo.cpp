41d3dbe4a19d12cfda774ae1587596631836cfaa
Attila M. Magyar <attila.m.magyar@gmail.com>
2023-10-30 23:10:53 +0100

    LFO: fix number of child `SignalProducer`s reported parent constructor

--- src/dsp/lfo.cpp
+++ src/dsp/lfo.cpp
@@ -32,7 +32,7 @@ namespace JS80P
 {
 
 LFO::LFO(std::string const name) noexcept
-    : SignalProducer(1, 10),
+    : SignalProducer(1, 11),
     waveform(name + "WAV", Oscillator_::SOFT_SQUARE),
     frequency(name + "FRQ", 0.01, 30.0, 1.0),
     phase(name + "PHS", 0.0, 1.0, 0.0),
