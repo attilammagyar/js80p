22b0aa7ed571c2e8d8b506b20ad70afe14a1ea61
Attila M. Magyar <attila.m.magyar@gmail.com>
2023-12-26 16:14:41 +0100

    scripts, make, gui: include GPL notice and copyright stuff in helper scripts, makefiles, etc.

--- scripts/bmp_to_png.py
+++ scripts/bmp_to_png.py
@@ -1,4 +1,20 @@
-#!/usr/bin/python3
+###############################################################################
+# This file is part of JS80P, a synthesizer plugin.
+# Copyright (C) 2023  Attila M. Magyar
+#
+# JS80P is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# JS80P is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+###############################################################################
 
 import os.path
 import sys
