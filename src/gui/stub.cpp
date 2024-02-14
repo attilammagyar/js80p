d6b703407510f216ec4ea942a5b0329a15cf48f0
Attila M. Magyar <attila.m.magyar@gmail.com>
2024-02-15 00:58:48 +0100

    gui: fix typo in `stub.cpp`

--- src/gui/stub.cpp
+++ src/gui/stub.cpp
@@ -15,8 +15,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-#ifndef JS80P__GUI_STUBS_CPP
-#define JS80P__GUI_STUBS_CPP
+#ifndef JS80P__GUI_STUB_CPP
+#define JS80P__GUI_STUB_CPP
 
 #include "js80p.hpp"
 #include "synth.hpp"
