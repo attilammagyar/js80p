403dcf63279dec6735ddfc0bb7ad737e3cecdd5a
Attila M. Magyar <attila.m.magyar@gmail.com>
2024-04-04 01:27:51 +0200

    Math: update year in copyright comment

--- tests/test_math.cpp
+++ tests/test_math.cpp
@@ -1,6 +1,6 @@
 /*
  * This file is part of JS80P, a synthesizer plugin.
- * Copyright (C) 2022  Attila M. Magyar
+ * Copyright (C) 2022, 2023, 2024  Attila M. Magyar
  *
  * JS80P is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
