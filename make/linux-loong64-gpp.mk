2bcb821bd028a30ad5c49c5f00f7697e159bedf6
Attila M. Magyar <attila.m.magyar@gmail.com>
2024-10-20 12:37:02 +0200

    make, scripts: update copyright headers for LoongArch related changes

--- make/linux-loong64-gpp.mk
+++ make/linux-loong64-gpp.mk
@@ -1,7 +1,6 @@
 ###############################################################################
 # This file is part of JS80P, a synthesizer plugin.
-# Copyright (C) 2023, 2024  Attila M. Magyar
-# Copyright (C) 2023  @aimixsaka (https://github.com/aimixsaka/)
+# Copyright (C) 2024  @YHStar (https://github.com/YHStar)
 #
 # JS80P is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
