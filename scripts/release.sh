f76d94a1432e896968725a7389d4853214414ea4
Attila M. Magyar <attila.m.magyar@gmail.com>
2025-01-12 14:01:56 +0100

    scripts: fix distortion docs archiving in `release.sh`

--- scripts/release.sh
+++ scripts/release.sh
@@ -118,8 +118,8 @@ main()
     cp --verbose --recursive \
         doc/pm-fm-equivalence.pdf \
         doc/pm-fm-equivalence.tex \
-        distortions.html \
-        distortions.ipynb \
+        doc/distortions.html \
+        doc/distortions.ipynb \
         "$DIST_DIR_BASE/$source_dir/doc/"
 
     find "$DIST_DIR_BASE/$source_dir/" -name ".*.swp" -delete
