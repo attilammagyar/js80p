72f4bf8759c65db426aa45c7276e1ee34ee510f0
Attila M. Magyar <attila.m.magyar@gmail.com>
2025-07-13 19:14:07 +0200

    scripts: create helper script for finding copyright header problems

---
+++ scripts/find_copyright_problems.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+###############################################################################
+# This file is part of JS80P, a synthesizer plugin.
+# Copyright (C) 2025  Attila M. Magyar
+#
+# JS80P is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# JS80P is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+###############################################################################
+
+main()
+{
+    local mod_year
+
+    find Makefile gui make scripts src tests -type f \
+      | grep -Eiv '\.(bmp|def|flac|png|rc|rpp|tpl|xcf)$' \
+      | while read
+        do
+            mod_year="$(git log -1 --format="%ci" -- "$REPLY" | cut -d"-" -f1)"
+            head -n30 -- "$REPLY" \
+              | grep "Copyright.*$mod_year" >/dev/null || printf "%s\n" "$REPLY"
+        done
+}
+
+
+main "$@"
+exit $?
